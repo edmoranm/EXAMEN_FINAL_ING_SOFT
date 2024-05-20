6332872f3be3968e98e2c8897ea1110fbd86c115
edanielmoram@gmail.com <edanielmoram@gmail.com>
2024-05-20 08:43:19 -0600

    creacion de la navbar de bootstrap

---
+++ templates/nabvar.php
@@ -0,0 +1,14 @@
+<nav class="navbar fixed-top navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="../vistas/personas.php"><img src="../images/personas.png" alt="Logo" width="30" height="30">INGRESOS</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0" style="--bs-scroll-height: 100px;">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-house-fill me-2"></i>INICIO</a>
+        </li>
+    </div>
+  </div>
+</nav>
