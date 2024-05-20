a68e291ba140e764d6ebd2fc9f25fe59e47a10dd
edanielmoram@gmail.com <edanielmoram@gmail.com>
2024-05-20 10:36:00 -0600

    le cambie color al navbar por uno mas vistoso para el usuario

--- templates/nabvar.php
+++ templates/nabvar.php
@@ -1,6 +1,6 @@
-<nav class="navbar fixed-top navbar-expand-lg bg-light">
+<nav class="navbar fixed-top navbar-expand-lg bg-success">
   <div class="container-fluid">
-    <a class="navbar-brand" href="../vistas/personas.php"><img src="../images/personas.png" alt="Logo" width="30" height="30">INGRESOS</a>
+    <a class="navbar-brand" href="../vistas/personas.php"><img src="../images/personas.png" alt="Logo" width="30" height="30" >INGRESOS</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
